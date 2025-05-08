a4fb58e0e02c99d2482041f6392507153e29490c
Ilya Zazvonov <Acer@DESKTOP-5LU3HEA>
2025-05-09 00:32:38 +0400

    Add main functionality add: window.py with main class for application add: utils.py with help functions for counting probabilities and extracting photo data

---
+++ utils.py
@@ -0,0 +1,33 @@
+from math import sqrt, exp, tan, pi
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+
+def count_P(Rвпис, Rопис, S, G, L, Lоб, Lф, lm, R, N, O, kпр):
+    O = 62*pi/180
+
+    K = abs(Lоб-Lф)/Lф
+    B = sqrt(G*(Rвпис + Rопис)/(2*S))
+
+    lпикс = 360*N*tan(lm/(2*R))/(kпр*pi*O)
+
+    Pобн = exp(-(B*L/(4*lпикс*sqrt(K)))**2)
+
+    Pрасп = exp(-(B*L/(lпикс*sqrt(K)))**2)
+
+    return Pобн, Pрасп
+
+
+def get_exif_data(image_path):
+    image = Image.open(image_path)
+    exif_data = {}
+    
+    if hasattr(image, '_getexif'):
+        exif_info = image._getexif()
+        if exif_info is not None:
+            for tag, value in exif_info.items():
+                decoded = TAGS.get(tag, tag)
+                exif_data[decoded] = value
+    
+    return exif_data
